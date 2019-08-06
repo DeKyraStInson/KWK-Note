cef8f5ea006438b54d271003a5dfbe36a979ac73
Apple <apple@Apples-MacBook-Pro-7.local>
2019-08-06 07:03:48 -0700

    initial commit

---
+++ kwk day1.playground/Contents.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+//let greeting = "Hello"
+////let is a constant (always stays the same)
+//
+////string interpulation
+//
+//print (" \(greeting) Kyra")
+//
+//
+//var first = "Kyra"
+//
+//var last = "Stinson"
+//
+//print("\(first) \(last)")
+//
+//
+////var interger = 4.0
+////var double = 5.0
+//
+//
+//print (interger * double)
+//var a = 12
+//var b = 65
+////var c = 31
+////var d = 98
+////
+////print ((a + b + c + d) / 4)
+//
+//
+//let Name = "Kyra"
+//var Address = "123 Sesame street"
+//let Birthday = "April 8th"
+//var GradeLevel = "9th grade"
+//var Age = "14"
+//
+//
+//7 == 7.0
+//
+//var luckynum = 7
+//luckynum < 10
+
+//var faveFood = "tacobell"
+//
+//if faveFood == "chipotle" {print ("🌯")}
+//else if faveFood == "starbucks" {print ("🥤")}
+//else if faveFood == "tacobell" {print ("Yumm!🌮")}
+
+//var hasAccount = true
+//if hasAccount == false {print ("Create an account!")}
+//if hasAccount == true {print ("Continue!")}
+
+//func walkDog() {
+//    print ("Get a dog")
+//    print ("Get a leash")
+//    print ("Put the leash on the dog")
+//    print ("Walk out the door with the dog")
+//}
+//    walkDog()
+
+//
+func cleanRoom(time: Int) {
+print ("pick up things off the ground")
+print ("vacuum the floor")
+print ("make the bed")
+print ("dust the room \(time)")
+//print ("organize the closet")
+//}
+//cleanRoom(time:20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
